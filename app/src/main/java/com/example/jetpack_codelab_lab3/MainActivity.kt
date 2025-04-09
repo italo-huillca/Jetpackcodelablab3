81de9035742ff66ce0b5df6661a98930cd672b41
Italo H <huillca.italo@gmail.com>
2025-04-08 22:48:13 -0500

    Eliminando funciones default

--- app/src/main/java/com/example/jetpack_codelab_lab3/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_codelab_lab3/MainActivity.kt
@@ -45,19 +45,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Jetpackcodelablab3Theme {
-        Greeting("Android")
-    }
-}
